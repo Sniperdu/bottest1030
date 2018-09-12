ce61fd2e2dfc407fff9a8785fdbd9778a8e3b66c
Sniperdu <42879823+Sniperdu@users.noreply.github.com>
2018-09-12 13:47:32 +0200

    Update addrole.js

--- .gitignore/addrole.js
+++ .gitignore/addrole.js
@@ -8,23 +8,23 @@ module.exports.run = async (bot, message, args) => {
 
   //!addrole @andrew Dog Person
 
-  if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("Sorry pal, you can't do that.");
+  if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("Désoler tu ne peut pas");
 
   let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
 
-  if(!rMember) return message.reply("Couldn't find that user, yo.");
+  if(!rMember) return message.reply("Je ne trouve pas le joueur.");
 
-  let role = args.join(" ").slice(22);
+  let role = args.join("Salut bienvenue").slice(22);
 
-  if(!role) return message.reply("Specify a role!");
+  if(!role) return message.reply("Role");
 
   let gRole = message.guild.roles.find(`name`, role);
 
-  if(!gRole) return message.reply("Couldn't find that role.");
+  if(!gRole) return message.reply("Je ne trouve pas ce role.");
 
 
 
-  if(rMember.roles.has(gRole.id)) return message.reply("They already have that role.");
+  if(rMember.roles.has(gRole.id)) return message.reply("Tu as déjà se role");
 
   await(rMember.addRole(gRole.id));
 
@@ -32,13 +32,13 @@ module.exports.run = async (bot, message, args) => {
 
   try{
 
-    await rMember.send(`Congrats, you have been given the role ${gRole.name}`)
+    await rMember.send(`Bravo tu as eu le role ${gRole.name}`)
 
   }catch(e){
 
     console.log(e.stack);
 
-    message.channel.send(`Congrats to <@${rMember.id}>, they have been given the role ${gRole.name}. We tried to DM them, but their DMs are locked.`)
+    message.channel.send(`Bravo à <@${rMember.id}>, pour avoir eu le role ${gRole.name}.`)
 
   }
 
